cd461eeadabeb9d277ae11b52636d2281ad93ad9
Michael Krämer <info.m.kraemer@gmail.com>
2024-07-15 16:39:38 +0200

    fullscreen integration

--- js/game.js
+++ js/game.js
@@ -84,3 +84,14 @@ window.addEventListener('keyup', (KeyboardEvent) => {
             break;
     }
 });
+
+
+$('#toggle_fullscreen').on('click', function(){
+    // if already full screen; exit
+    // else go fullscreen
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      $('#canvas').get(0).requestFullscreen();
+    }
+  });
